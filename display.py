13b8203df6cb347ca4692b6e926594cd8f78ce71
Mohamed Mussa <mussaop812@gmail.com>
2026-09-10 21:50:20 -0400

    updated the display

--- display.py
+++ display.py
@@ -2,8 +2,9 @@
 # Author: Mohamed Mussa
 
 def welcome():
-    print("==============================")
-    print("      SCHEDULE GENERATOR")
-    print("==============================")
-    print("Welcome to the Schedule Generator!")
+    print("\n" * 5)
+    print("SCHEDULE GENERATOR".center(50))
+    print("=" * 50)
     print()
+    print("Welcome to the Schedule Generator!".center(50))
+    print("\n" * 5)
