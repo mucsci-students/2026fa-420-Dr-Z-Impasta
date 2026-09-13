1c21a0c12cfb76d7c2befda70d6bbd0cd81bbc70
Mohamed Mussa <mussaop812@gmail.com>
2026-09-13 17:59:57 -0400

    Add welcome page

---
+++ src/zimpasta/welcome_page.py
@@ -0,0 +1,15 @@
+"File: Welomce_page"
+"Author: Mohamed Mussa"
+
+"""Welcome page shown at startup, before the main menu."""
+
+from zimpasta.console import Console
+
+
+def welcome(console: Console) -> None:
+    console.say("\n" * 5)
+    console.say("SCHEDULE GENERATOR".center(50))
+    console.say("=" * 50)
+    console.say("")
+    console.say("Welcome to the Schedule Generator!".center(50))
+    console.say("\n" * 5)
