b5af47019976f5ec1c1cbe0ee452cd873e292a20
Foster VanFleet <fvfleet@gmail.com>
2026-09-16 15:36:35 -0400

    Config_Finder style fix

--- src/zimpasta/config_finder.py
+++ src/zimpasta/config_finder.py
@@ -22,44 +22,22 @@ class ConfigFinder:
     def parse_config(
         self,
         config_type: (
-            type[CourseConfig]
-            | type[FacultyConfig]
-            | type[LabConfig]
-            | type[RoomConfig]
+            type[CourseConfig] | type[FacultyConfig] | type[LabConfig] | type[RoomConfig]
         ),
         identifier: str,
-    ) -> (
-        list[CourseConfig]
-        | list[FacultyConfig]
-        | list[LabConfig]
-        | list[RoomConfig]
-    ):
+    ) -> list[CourseConfig] | list[FacultyConfig] | list[LabConfig] | list[RoomConfig]:
         if config_type is CourseConfig:
             return [
-                course
-                for course in self.config.config.courses
-                if course.course_id == identifier
+                course for course in self.config.config.courses if course.course_id == identifier
             ]
 
         if config_type is FacultyConfig:
-            return [
-                faculty
-                for faculty in self.config.config.faculty
-                if faculty.name == identifier
-            ]
+            return [faculty for faculty in self.config.config.faculty if faculty.name == identifier]
 
         if config_type is RoomConfig:
-            return [
-                room
-                for room in self.config.config.rooms
-                if room.name == identifier
-            ]
+            return [room for room in self.config.config.rooms if room.name == identifier]
 
         if config_type is LabConfig:
-            return [
-                lab
-                for lab in self.config.config.labs
-                if lab.name == identifier
-            ]
+            return [lab for lab in self.config.config.labs if lab.name == identifier]
 
         raise TypeError(f"Unsupported configuration type: {config_type}")
